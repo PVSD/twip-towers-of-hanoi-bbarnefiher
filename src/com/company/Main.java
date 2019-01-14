aa57618faf4865658353466d2d5a607647de3d2f
bbarnefiher <bb222@pvsd.org>
2019-01-14 11:21:26 -0500

    end of class btw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
-
+import java.io.*;
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Tower of Hanoi Game.\n" +
+                "This program will solve this puzzle for you.\n" +
+                "Please input number of disks to solve for:");
+        Scanner intReader = new Scanner(System.in);
+        int diskCount = intReader.nextInt();
+        TowersOfHanoi Towers = new TowersOfHanoi(diskCount);
+        System.out.println("Instructions:");
+        int totalTurns = (int)(Math.pow(2, diskCount) - 1);
+        for (int i = 1; i < totalTurns + 1; i++) {
+
+            System.out.println(i + ": " + Towers.solve() + ".");
+        }
+
+        System.out.println("The game has ended.");
     }
 }
---
+++ src/com/company/TowersOfHanoi.java
@@ -0,0 +1,30 @@
+package com.company;
+
+/**
+ * Created by bb222 on 1/14/19.
+ */
+public class TowersOfHanoi {
+    boolean done = false;
+    int numOfDisks = 0;
+    public TowersOfHanoi(int numOfDisks){
+        this.numOfDisks = numOfDisks;
+
+    //    if (){
+    //        done = true;
+    //    }
+    }
+
+    public String solve(){
+        return "yeet";
+    }
+
+
+    public boolean done(){
+        if (done = true){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
